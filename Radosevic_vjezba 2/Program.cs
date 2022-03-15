e42075a29ab90c3556deaecc04aea6208240cf0e
mev <mev@PC-19-25>
2022-03-15 15:50:07 +0100

    Add project files.

---
+++ Radosevic_vjezba 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Radosevic_vjezba_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
